5b1622a7a0a83338a52818a1f391c8071744d4a4
Dragan <101023448@student.swin.edu.au>
2018-08-03 12:12:13 +1000

    Default Implementation

---
+++ ReleasePipelineDemo/ReleasePipelineDemoApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ReleasePipelineDemoApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
